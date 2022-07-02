aa1f7b9ca1576e6e1ceed535338520d621b9ecd5
BlackMalaika <103142787+BlackMalaika@users.noreply.github.com>
2022-07-02 13:30:04 +0100

    initial commit

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is my favourite colour?",
+    answer: "Sky Blue",
+    options: [
+      "Lavender",
+      "Sky Blue",
+      "Black",
+      "Pink"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What was my least favourite subject in school?",
+    answer: "Physics",
+    options: [
+      "Economics",
+      "Chemistry",
+      "Biology",
+      "Physics"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How do you think I deal with difficult situations?",
+    answer: "Cry and Pray",
+    options: [
+      "Cry and Pray",
+      "Get drunk and forget my problems",
+      "Keep my issues to myself and become distant",
+      "All of these"
+    ]
+  },
+    {
+    numb: 4,
+    question: "When do I usually brush?",
+    answer: "In the morning before breakfast",
+    options: [
+      "Later in the day",
+      "At night", 
+      "After breakfast",
+      "In the morning before breakfast"
+    ]
+  },
+    {
+    numb: 5,
+    question: "When is my Birthday?",
+    answer: "September 27th",
+    options: [
+      "September 27th",
+      "September 26th",
+      "September 10th",
+      "August 31st"
+    ]
+  },
+];
